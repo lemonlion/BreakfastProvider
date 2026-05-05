a6a7b67a1d7cce719e706c46091e3555db5d1d52
Aryeh Citron <aryehcitron@gmail.com>
2026-05-05 13:26:04 +0100

    chore: upgrade TTD to 2.29.9-beta, remove obsolete ReportWritersConfiguration()

--- tests/BreakfastProvider.Tests.Component.LightBDD/Infrastructure/ConfiguredLightBddScopeAttribute.cs
+++ tests/BreakfastProvider.Tests.Component.LightBDD/Infrastructure/ConfiguredLightBddScopeAttribute.cs
@@ -43,7 +43,7 @@ public class ConfiguredLightBddScopeAttribute : LightBddScope
             ThreadPool.SetMinThreads(100, 100);
 
         var testAssembly = Assembly.GetAssembly(typeof(ConfiguredLightBddScopeAttribute))!;
-        configuration.ReportWritersConfiguration().CreateStandardReportsWithDiagrams(
+        configuration.CreateStandardReportsWithDiagrams(
                 new ReportConfigurationOptions
                 {
                     SpecificationsTitle = "Breakfast Provider Specifications",
